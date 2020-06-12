da132002d1d88a37c231a12581881e159010e30c
Dlynn2 <35791802+Dlynn2@users.noreply.github.com>
2020-06-12 12:38:55 -0600

    landing image placed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import testing from './test2.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="landing-container">
+          <header className="overlay">I'm Dylan Lynn.
+          <br />
+          A Developer
+          </header>
+          <img src={testing} className="landing-image" />
+        </div>
       </header>
     </div>
   );
---
+++ src/components/About.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+function About() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="landing-container">
+            <header className="overlay">I'm Dylan Lynn.
+            <br />
+            A Developer
+            </header>
+            {/* <img src={testing} className="landing-image" /> */}
+          </div>
+        </header>
+      </div>
+    );
+  }
+  
+  export default About;
